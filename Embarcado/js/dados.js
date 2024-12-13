66961f834a5180bee16e2d448dd243300f92fa3d
Leonardo Souza <leonardo.castrodesouza@gmail.com>
2024-12-13 16:25:17 -0300

    bug: Imagens não Carregam

--- Embarcado/js/dados.js
+++ Embarcado/js/dados.js
@@ -1,47 +1,47 @@
-{
+let dados = {
     "produtos": [
       {
-        "imagem": "/Embarcado/img/produtos/Cpu1821.jpg",
+        "imagem": "img/produtos/Cpu1821.jpg",
         "nome": "CPU DNP 1821",
         "preco": "R$ 1800,00"
       },
       {
-        "imagem": "/Embarcado/img/produtos/Cpu1842.jpg",
+        "imagem": "img/produtos/Cpu1842.jpg",
         "nome": "CPU DNP 1842",
         "preco": "R$ 1800,00"
       },
       {
-        "imagem": "/Embarcado/img/produtos/Sensor0460.jpg",
+        "imagem": "img/produtos/Sensor0460.jpg",
         "nome": "Sensor DNP 0460",
         "preco": "R$ 1700,00"
       },
       {
-        "imagem": "/Embarcado/img/produtos/KitSobreChassi.jpg",
+        "imagem": "img/produtos/KitSobreChassi.jpg",
         "nome": "Kit Sobre Chassi",
         "preco": "R$ 2800,00"
       },
       {
-        "imagem": "/Embarcado/img/produtos/SensorBasculaAlta.jpg",
+        "imagem": "img/produtos/SensorBasculaAlta.jpg",
         "nome": "Sensor de Bascula Alta",
         "preco": "R$ 180,00"
       },
       {
-        "imagem": "/Embarcado/img/produtos/Pressostato.jpg",
+        "imagem": "img/produtos/Pressostato.jpg",
         "nome": "Pressostato",
         "preco": "R$ 80,00"
       },
       {
-        "imagem": "/Embarcado/img/produtos/ValvSolParker.jpg",
+        "imagem": "img/produtos/ValvSolParker.jpg",
         "nome": "Valvula Solenoide Parker",
         "preco": "R$ 490,00"
       },
       {
-        "imagem": "/Embarcado/img/produtos/FimDeCursoMetaltex.jpg",
+        "imagem": "img/produtos/FimDeCursoMetaltex.jpg",
         "nome": "Fim de Curso Metaltex",
         "preco": "R$ 320,00"
       },
       {
-        "imagem": "/Embarcado/img/produtos/Cabo10Mts.jpg",
+        "imagem": "img/produtos/Cabo10Mts.jpg",
         "nome": "Cabo de 10 Mts Sensor",
         "preco": "R$ 90,00"
       }
